b3ac75123b0f2c532903fa319878960b736e8b47
Bryan-Elam <elam.bryan@gmail.com>
2021-07-06 01:37:36 -0400

    finished javascript

--- js/metric.js
+++ js/metric.js
@@ -1,20 +1,19 @@
-
-
-
-
-
-
-function myFunction() {
-  console.log("Hello World!");
-  //let temp = document.getElementById('celsius').value;
-  //var converted = value + result;
-  //document.getElementById('farenheit').innerHTML(temp);
+function convertDistance() {
+    let d = document.getElementById('naut').value;
+    let milesConverted = d * 1.151;  //conversion formula
+    document.getElementById('miles').value = milesConverted;
 }
 
-function someOtherFunction() {
-  alert ("This function was also executed!")
+    let dist = document.getElementById('distance');
+    dist.addEventListener("click", convertDistance);
+
+function convertTemp() {
+  
+  let t = document.getElementById('celsius').value;
+  let converted = t * 1.8 + 32;  //formula for conversion
+ 
+  document.getElementById('fahrenheit').value = converted;
 }
-let temp = document.getElementById('temperature');
-temp.addEventListener("click", myFunction);
-temp.addEventListener("click", someOtherFunction);
-//x.addEventListener('click', myFunction);
+
+  let temp = document.getElementById('temperature');
+  temp.addEventListener("click", convertTemp);
